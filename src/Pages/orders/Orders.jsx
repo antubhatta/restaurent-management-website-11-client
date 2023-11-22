9465e52f01915e3f97a0304095fc2315487e5137
antubhatta <antubhatta09@gmail.com>
2023-11-23 03:11:19 +0600

    order page updated

--- src/Pages/orders/Orders.jsx
+++ src/Pages/orders/Orders.jsx
@@ -12,42 +12,42 @@ const Orders = () => {
   if (orders?.length === 0)
     return (
       <div className="mt-28 font-medium w-full min-h-[60vh] flex items-center justify-center">
-        <p>You haven&apos;t any order yet.</p>
+        <p>You haven&apos;t placed any orders yet.</p>
       </div>
     );
 
-  console.log(orders);
-
   return (
-    <div className="container max-w-5xl mx-auto px-3 md:px-0 mt-20 p-8 space-y-2 ">
+    <div className="container max-w-5xl mx-auto px-3 md:px-0 mt-20 p-8 space-y-4 bg-gray-100 rounded-lg shadow-lg">
       <Helmet>
-        <title>Orders - EatsHub</title>
+        <title>Your Orders - EatsHub</title>
       </Helmet>
-      <h1 className="text-2xl font-bold text-center mb-10">Your Orders</h1>
+      <h1 className="text-3xl font-bold text-center mb-6 text-blue-600">
+        Your Orders
+      </h1>
       {orders?.map((order) => (
         <div
           key={order._id}
-          className="w-full flex items-center justify-between space-x-8"
+          className="w-full flex items-center justify-between space-x-8 bg-white p-4 rounded-md shadow-md"
         >
           <img
             src={order.imageUrl}
             alt={order.name}
-            className="h-16 w-20 rounded-sm"
+            className="h-16 w-20 rounded-md object-cover"
           />
-          <div>
-            <h3 className="text-lg font-medium mb-1 text-dark-2">
+          <div className="flex flex-col">
+            <h3 className="text-lg font-medium mb-1 text-gray-800">
               {order.name}
             </h3>
-            <div className="space-x-2 text-dark-3">
-              <span>{order.quantity} items</span>
+            <div className="flex items-center text-gray-600">
+              <span className="mr-2">{order.quantity} items</span>
               <span>{order.price}</span>
             </div>
           </div>
           <button
             onClick={() => deleteOrder(order._id)}
-            className="text-red-500 ml-auto"
+            className="text-red-500 ml-auto focus:outline-none"
           >
-            <BsTrash />
+            <span className="md:text-xl"><BsTrash /></span>
           </button>
         </div>
       ))}
